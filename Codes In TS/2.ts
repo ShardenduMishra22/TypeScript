b0cae2792a5bf6db8c509e9e266bff71c4c51414
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-14 02:58:55 +0530

    Learning TypeScript

---
+++ Codes In TS/2.ts
@@ -0,0 +1,13 @@
+let NumOne = 3;
+let NumTwo = "2";
+
+let Sum = NumOne + NumTwo;
+
+console.log(Sum);
+
+let Name : string = "Shardendu Mishra";
+
+Name = "2";
+// Name = 2; Error
+
+export {}
---
+++ Codes In TS/3.ts
@@ -0,0 +1,25 @@
+// let Greeting : string = "Hi Man";
+// Greeting.toLowerCase();
+// console.log(Greeting);
+
+let Greeting : number = 4;
+console.log(Greeting);
+// Greeting.toLowerCase(); will show an error 
+
+let num = 42;
+/* 
+num. => this will show 
+        all properties of number type of data
+*/
+
+let hero : string;
+function GetHero(){
+    return "Hero";
+}
+hero = GetHero();
+console.log(hero);
+
+let IsLoggedIn : boolean = true;
+console.log(IsLoggedIn);
+
+export {};

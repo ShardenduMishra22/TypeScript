c95e7e52f6fbb8de184aaacec1eeca6bcd700937
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-14 03:33:48 +0530

    Leaning TypeScript

---
+++ Codes In TS/6.ts
@@ -0,0 +1,12 @@
+let a = ((name : string) : string => {
+    return (`This function can only return a string and you're ${name}!!`);
+});
+
+console.log(a("Shardendu Mishra"));
+
+let Name = ["Shardendu","Mishra","Pandit","Ji"]
+Name.map((name) => {
+    console.log(`${name} is Present !!`);
+});
+
+export {};

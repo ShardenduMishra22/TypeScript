b9f94b34dc4cb1a8489f3eac503895a41bec9573
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-14 15:08:03 +0530

    Learning TypeScript

---
+++ Codes In TS/9.ts
@@ -0,0 +1,15 @@
+type User = {
+    readonly _id : string
+    name : string 
+    email : string
+    isActive : boolean
+}
+
+let MyUser = {
+    _id : "1",
+    name : "Shardendu Mishra",
+    email : "12@23.com",
+    isActive : true,
+}
+
+export {};

bbefce53ddef6aee6831a55e0f55f52233d7660a
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-14 19:47:55 +0530

    Learning TypeScript

--- Codes In TS/18.ts
+++ Codes In TS/18.ts
@@ -1,3 +1,30 @@
 class User {
-    
+    email: string;
+    private name: string;
+    readonly city: string;
+
+    constructor(email: string, name: string, city: string = "Jaipur") {
+        this.email = email;
+        this.name = name;
+        this.city = city;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    setName(NewName: string): void {
+        this.name = NewName; 
+    }
 }
+
+let u1 = new User('shardendumishra01@gmail.com', 'Shardendu Mishra');
+console.log(u1);
+console.log(u1.getName());
+
+u1.setName('Shardendu');
+console.log(u1.getName());
+
+let u2 = new User('shardendumishra01@gmail.com', 'Shardendu Mishra', 'Kanpur');
+console.log(u2);
+console.log(u2.city);

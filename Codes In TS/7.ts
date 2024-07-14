f69b2d5ebf78dfada551b955c98ee60f23918716
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-14 14:21:09 +0530

    Learning TypeScript

---
+++ Codes In TS/7.ts
@@ -0,0 +1,12 @@
+// let Student = ((err) : never => {
+//     throw new Error(err);
+// });
+
+function CreateUser({name : string ,age : number ,IsPaid : boolean}){
+    return null;
+}
+
+let user = {name : "Shardendu Mishra",age : 21,IsPaid : true};
+CreateUser(user);
+
+export {};

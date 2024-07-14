d81859f2c4dae76866a66c82051fcd3766d30050
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-14 15:37:31 +0530

    Learning TypeScript

---
+++ Codes In TS/11.ts
@@ -0,0 +1,20 @@
+let Score : number | string ;
+
+Score = 341
+Score = "2"
+
+type User = {
+    name : string;
+    id : number;
+}
+
+type Admin = {
+    username : string;
+    id : number;
+}
+
+let Shardendu : (User | Admin) = {
+    username : "Shardendu Mishra",
+    id : 122,
+
+}  
